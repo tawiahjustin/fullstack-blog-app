aec9a2534fd32f2f94b223cb47321b4e98db6d26
tawiahjustin <121239070+tawiahjustin@users.noreply.github.com>
2023-06-18 01:37:25 +0000

    Added edit and update

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { formatISO9075 } from 'date-fns'
 
-const Post = ({ title, summary, content, cover, createdAt, author }) => {
+const Post = ({ _id, title, summary, content, cover, createdAt, author }) => {
   return (
     <div className='post'>
       <div className='image'>
-        <img src={'http://localhost:4000/' + cover} alt='' />
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/' + cover} alt='' />
+        </Link>
       </div>
       <div className='text'>
-        <h2>{title}</h2>
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
         <p className='info'>
           <a className='author'>{author.username}</a>
           <time>{formatISO9075(new Date(createdAt))}</time>
--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -1,24 +1,38 @@
-import React, { useState } from 'react'
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
+import React, { useEffect, useState } from 'react'
+import { Navigate, useParams } from 'react-router-dom'
 import Editor from '../Editor'
-import { Navigate } from 'react-router-dom'
 
-function CreatePost() {
+const EditPost = () => {
+  const { id } = useParams()
   const [title, setTitle] = useState('')
   const [summary, setSummary] = useState('')
   const [content, setContent] = useState('')
   const [files, setFiles] = useState('')
   const [redirect, setRedirect] = useState(false)
-  async function createNewPost(e) {
+
+  useEffect(() => {
+    fetch('http://localhost:4000/post/' + id).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title)
+        setContent(postInfo.content)
+        setSummary(postInfo.summary)
+      })
+    })
+  }, [])
+
+  async function updatePost(e) {
+    e.preventDefault()
     const data = new FormData()
     data.set('title', title)
     data.set('summary', summary)
     data.set('content', content)
-    data.set('file', files[0])
-    e.preventDefault()
+    data.set('id', id)
+    if (files?.[0]) {
+      data.set('file', files?.[0])
+    }
+
     const response = await fetch('http://localhost:4000/post', {
-      method: 'POST',
+      method: 'PUT',
       body: data,
       credentials: 'include',
     })
@@ -26,12 +40,12 @@ function CreatePost() {
       setRedirect(true)
     }
   }
-
   if (redirect) {
-    return <Navigate to={'/'} />
+    return <Navigate to={'/post/' + id} />
   }
+
   return (
-    <form onSubmit={createNewPost}>
+    <form onSubmit={updatePost}>
       <input
         type='title'
         placeholder='Title'
@@ -45,10 +59,10 @@ function CreatePost() {
         onChange={(e) => setSummary(e.target.value)}
       />
       <input type='file' onChange={(e) => setFiles(e.target.files)} />
-      <Editor value={content} onChange={setContent} />
-      <button style={{ marginTop: '5px' }}>Create post</button>
+      <Editor onChange={setContent} value={content} />
+      <button style={{ marginTop: '5px' }}>Update post</button>
     </form>
   )
 }
 
-export default CreatePost
+export default EditPost
